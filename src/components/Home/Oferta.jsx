b51dc53a2def7586d461a1ca842fef4849ffeeb4
xYaure <sebastianklak6@gmail.com>
2023-10-31 16:38:54 +0100

    #

--- src/components/Home/Oferta.jsx
+++ src/components/Home/Oferta.jsx
@@ -1,8 +1,93 @@
 import React from 'react'
+import './oferta.css'
+import tenis from '../../assets/oferty_img/tenis.jpg'
+import szkola from '../../assets/oferty_img/szkola.jpeg'
+import kalendarz from '../../assets/oferty_img/kalendarz.jpg'
+import koszykowka from '../../assets/oferty_img/koszykowka.jpg'
+import pingpong from '../../assets/oferty_img/pingpong.jpg'
+import pilka from '../../assets/oferty_img/pilka.jpg'
 
 const Oferta = () => {
   return (
-    <div>Oferta</div>
+    <section className='oferta__container'>
+      <div className="oferta__title">
+        <h1>Nasza oferta</h1>
+      </div>
+      <div className="oferty">
+        <div className="oferta">
+          <div className="oferta__zdj">
+            <img className='oferta__zdjecie' src={tenis} alt="" />
+            <div className="oferta__napis">
+              <h2>Kurs 5+</h2>
+            </div>
+          </div>
+          <div className="oferta__nazwa">
+            <h3>1 Kupon Kurs 5+ <span>44.99 zł</span></h3>
+          </div>
+          <button className='btn'>Zamów</button>
+        </div>
+        <div className="oferta">
+          <div className="oferta__zdj">
+            <img className='oferta__zdjecie' src={pilka} alt="" />
+            <div className="oferta__napis">
+              <h2>Kurs 10+</h2>
+            </div>
+          </div>
+          <div className="oferta__nazwa">
+            <h3>1 Kupon kurs 10+ <span>99.99 zł</span></h3>
+          </div>
+          <button className='btn'>Zamów</button>
+        </div>
+        <div className="oferta">
+          <div className="oferta__zdj">
+            <img className='oferta__zdjecie' src={pingpong} alt="" />
+            <div className="oferta__napis">
+              <h2>Kurs 3.5+</h2>
+            </div>
+          </div>
+          <div className="oferta__nazwa">
+            <h3>1 kupon kurs 3.5+ <span>29.99 zł</span></h3>
+          </div>
+          <button className='btn'>Zamów</button>
+        </div>
+        <div className="oferta">
+          <div className="oferta__zdj">
+            <img className='oferta__zdjecie' src={koszykowka} alt="" />
+            <div className="oferta__napis">
+              <h2>3 Kupony</h2>
+            </div>
+          </div>
+          <div className="oferta__nazwa">
+            <h3>Pakiet 3 Kupony <span>79.99 zł</span></h3>
+          </div>
+          <button className='btn'>Zamów</button>
+        </div>
+        <div className="oferta">
+          <div className="oferta__zdj">
+            <img className='oferta__zdjecie' src={kalendarz} alt="" />
+            <div className="oferta__napis">
+              <h2>30 dni</h2>
+            </div>
+          </div>
+          <div className="oferta__nazwa">
+            <h3>Subskrybcja 30 dni  <span>249 zł</span></h3>
+          </div>
+          <button className='btn'>Zamów</button>
+        </div>
+        <div className="oferta">
+          <div className="oferta__zdj">
+            <img className='oferta__zdjecie' src={szkola} alt="" />
+            <div className="oferta__napis">
+              <h2>Nauka</h2>
+            </div>
+          </div>
+          <div className="oferta__nazwa">
+            <h3>Nauka typowania <span>799 zł</span></h3>
+          </div>
+          <button className='btn'>Zamów</button>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -3,6 +3,8 @@ import { Outlet, Link } from "react-router-dom";
 import './menu.css'
 import buttons from './buttons';
 import Stopka from '../Stopka/Stopka';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
+
 
 const Menu = () => {
     
@@ -12,22 +14,28 @@ const Menu = () => {
         activeElement && setActiveId(id)
     }
 
+    const [active, setActive] = useState(false);
+
     return (
         <>
         <nav className='menu__container'>
             <Link to="/">
                 <h1 className='menu__logo' onClick={() => setActiveId(null)}>EzTYP</h1>
             </Link>
-            <ul className='menu__list'>
+            <ul className={active ? "menu__list menu__reveal" : "menu__list"}>
                 {buttons.map(({id, text, path}) =>{
                     return(
                         <li key={id} className={id === activeId ? 'active' : ''}>
-                            <Link to={path} onClick={handleClick(id)} >{text}</Link>
+                            <Link to={path} onClick={handleClick(id), ()=> setActive(false)} >{text}</Link>
                         </li>
                     )
                 })}
+                <i><AiOutlineClose onClick={()=> setActive(!active)} /></i>
             </ul>
             <Link to="/regulamin" onClick={() => setActiveId(null)} className='menu__regulamin'>Regulamin</Link>
+            <div className="menu__toggle">
+                <i><AiOutlineMenu onClick={()=> setActive(!active)} /></i>
+            </div>
         </nav>
 
         <Outlet />
